9b06c65049c6940f568fb59e7d46c8a20eff87a8
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-06-02 13:39:26 -0300

    Mudança da extensão do arquivo

---
+++ site/feed.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/cards.css">
+</head>
+<body class="cardsContainer">
+    <div class="cardBox">
+        <img src="img/img4.jpg" alt="">
+        <h2>Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+    </div>
+
+    <div class="flexCardsContainer">
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quos vel eveniet necessitatibus incidunt architecto, cumque tempore, eum voluptatum dolore doloribus? Qui delectus necessitatibus voluptatibus tempore saepe odio dicta ipsum?</p>
+        </div>
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+        </div>
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+        </div>
+
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+        </div>
+
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+        </div>
+
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+        </div>
+
+        <div class="cardBox">
+            <img src="img/img4.jpg" alt="">
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum, soluta consectetur saepe atque eos veritatis architecto quos similique voluptatum veniam doloribus impedit nesciunt accusamus? Placeat laborum laudantium nemo ab.</p>
+        </div>
+    </div>
+</body>
+</html>
